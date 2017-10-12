589893df5cc276ac1d2b113e25e41d4b47803719
awafa28 <awafa@swipezoom.com>
2017-10-12 16:13:59 +0400

    2

--- src/test/java/sauce.java
+++ src/test/java/sauce.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import java.util.LinkedList;
 import java.net.URL;
-
 import com.saucelabs.junit.ConcurrentParameterized;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
